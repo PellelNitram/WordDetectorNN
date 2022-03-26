8a7a194880e7041eb50f5c0bddfe71cee7b976fb
Martin L <konten.ma.le@gmail.com>
2022-03-26 10:01:15 +0100

    Add custom infer script.

---
+++ src/infer_martin.py
@@ -0,0 +1,56 @@
+import argparse
+from pathlib import Path as Path2
+
+import torch
+from path import Path
+import cv2
+
+from dataloader import DataLoaderImgFile
+from eval import evaluate
+from net import WordDetectorNet
+from visualization import visualize_and_plot
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--device', choices=['cpu', 'cuda'], default='cuda')
+    parser.add_argument('--weights-path', required=True, type=lambda p: Path2(p).absolute())
+    parser.add_argument('--images-path', required=True, type=lambda p: Path2(p).absolute())
+    parser.add_argument('--outdir', required=True, type=lambda p: Path2(p).absolute())
+    parser.add_argument('--plot', action='store_true')
+    args = parser.parse_args()
+
+    net = WordDetectorNet()
+    net.load_state_dict(torch.load( str(args.weights_path) , map_location=args.device))
+    net.eval()
+    net.to(args.device)
+
+    loader = DataLoaderImgFile(Path( str(args.images_path) ), net.input_size, args.device)
+    res = evaluate(net, loader, max_aabbs=1000)
+
+    for i, (img, aabbs) in enumerate(zip(res.batch_imgs, res.batch_aabbs)):
+        f = loader.get_scale_factor(i)
+        aabbs = [aabb.scale(1 / f, 1 / f) for aabb in aabbs]
+        img = loader.get_original_img(i)
+        if args.plot:
+            visualize_and_plot(img, aabbs)
+
+        # Normalise
+        img_min, img_max = img.min(), img.max()
+        img = ( img - img_min ) / ( img_max - img_min )
+        img *= 255
+        print(type(img), img.shape)
+        print(img.min(), img.max())
+
+        for ii, aabb in enumerate(aabbs):
+            aabb_rounded = aabb.enlarge_to_int_grid()
+            print('save:', aabb_rounded)
+
+            img_cropped = img[int(aabb_rounded.ymin):int(aabb_rounded.ymax),
+                              int(aabb_rounded.xmin):int(aabb_rounded.xmax)]
+
+            cv2.imwrite(str( args.outdir / f'image{i}_nr{ii}.jpg' ), img_cropped)
+
+
+if __name__ == '__main__':
+    main()
